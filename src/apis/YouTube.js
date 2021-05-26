561c7a18fd6b41eccfbd78e52f21298f448e7c6a
Will Daughtridge <wdaughtridge@gmail.com>
2021-05-26 11:33:00 -0400

    Update YouTube.js

--- src/apis/YouTube.js
+++ src/apis/YouTube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAgfgEVnmpMVnyD8MtoQ-J3cvxurJbuopw'
+const KEY = 'AIzaSyAgfgEVnmpMVnyD8MtoQ-J3cvxurJbuopw' // key is restricted in console.
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
